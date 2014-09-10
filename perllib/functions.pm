45d07e36b2b924c6acc2ad1535194c5c2bff032c
Hansaim Lim <hansaimlim@gmail.com>
2014-09-10 09:57:21 -0400

    this file will not be included in the final module

--- perllib/functions.pm
+++ perllib/functions.pm
@@ -18,23 +18,6 @@ sub get_GeneSymbol_by_ENSP
 	
 }
 
-sub make_dir
-{
-	#create a directory unless already created
-	my $dir = shift @_;
-	unless (-e $dir or mkdir $dir){
-		die "Unable to create $dir\n";
-	}
-	return;
-}
-
-sub dirname_add_slash
-{	
-	#add a slash at the end of a directory name unless already attached
-	my $dir = shift @_;
-	$dir .= '/' unless $dir =~ m/\/$/;
-	return $dir;
-}
 
 sub rm_special_char_in_drugname
 {
