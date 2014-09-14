9ef56cbe26f09d7635577622b2042d8456575dfe
Hansaim Lim <hansaimlim@gmail.com>
2014-09-14 02:00:58 -0400

    method to reverse simple hash added

--- perllib/DrugTargetBase.pm
+++ perllib/DrugTargetBase.pm
@@ -3,7 +3,7 @@ package DrugTargetBase;
 
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT = qw(chomp_array unique make_dir dirname_add_slash);
+@EXPORT = qw(chomp_array unique make_dir dirname_add_slash reverse_simple_hash);
 
 sub chomp_array
 {
@@ -44,6 +44,21 @@ sub dirname_add_slash
         my $dir = shift @_;
         $dir .= '/' unless $dir =~ m/\/$/;
         return $dir;
+}
+sub reverse_simple_hash
+{
+        #input: a hash reference
+        #output: a reference to the reverse hash
+        #warning: the original hash must be 1 dimensional--$hash{key} = scalar value
+        #warning: the key value pairs must be 1 to 1 relationship; possible loss of information otherwise
+        my $hash_ref = shift @_;
+        my %hash = %$hash_ref;
+        my %reverse_hash;
+        foreach my $key (keys %hash){
+                my $val = $hash{$key};
+                $reverse_hash{$val} = $key;
+        }
+        return \%reverse_hash;
 }
 
 1;
