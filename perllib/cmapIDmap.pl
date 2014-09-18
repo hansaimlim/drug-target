e0a1a655e7c5fb23e8c4391fe378c3d4755c05a8
Hansaim Lim <hansaimlim@gmail.com>
2014-09-18 05:06:12 -0400

    short script to output InChIKey - drugname list for cMap

---
+++ perllib/cmapIDmap.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use cMap;
+use IDMAP;
+use DrugTargetBase;
+use strict;
+use warnings;
+
+my $cmap50up = new cMap("50up");      #the range (50up) does not matter for extraction of common drugs
+my $ref = $cmap50up->get_cMap_InChIKey();
+my @arr = @$ref;
+my $file = "./static/cMap/InChIKey_drugname.tsv";
+open my $FH, '>', $file or die "Could not open cmapfile $file: $!\n";
+foreach my $ikey (@arr){
+        print $FH $ikey, "\t";
+        print $FH $cmap50up->get_cMap_drugname_by_InChIKey($ikey), "\n";
+}
+close $FH;
+exit;
