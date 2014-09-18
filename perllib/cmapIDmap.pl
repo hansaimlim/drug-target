4d98ba528286ab650596483b27c577c9b57e6fda
Hansaim Lim <hansaimlim@gmail.com>
2014-09-18 05:09:52 -0400

    output filename changed

--- perllib/cmapIDmap.pl
+++ perllib/cmapIDmap.pl
@@ -8,7 +8,7 @@ use warnings;
 my $cmap50up = new cMap("50up");      #the range (50up) does not matter for extraction of common drugs
 my $ref = $cmap50up->get_cMap_InChIKey();
 my @arr = @$ref;
-my $file = "./static/cMap/InChIKey_drugname.tsv";
+my $file = "./static/idmap/cMap_InChIKey_drugname.tsv";
 open my $FH, '>', $file or die "Could not open cmapfile $file: $!\n";
 foreach my $ikey (@arr){
         print $FH $ikey, "\t";
