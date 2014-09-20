886c007b09fdad509a8b05a6154d0ef2cbace58c
Hansaim Lim <hansaimlim@gmail.com>
2014-09-20 01:31:37 -0400

    UniProtKB_genename() methods used instead of indirect method -- reduce time

--- perllib/String_convert_genename.pl
+++ perllib/String_convert_genename.pl
@@ -10,6 +10,7 @@ $file = "./static/String/9606.protein.links.v9.1-GS-dist_demo.txt" if $is_demo_o
 my $outfile = "./static/String/9606.protein.links.v9.1-GN-dist.txt";
 $outfile = "./static/String/9606.protein.links.v9.1-GN-dist_demo.txt" if $is_demo_on == 1;
 
+my $ug_ref = UniProtKB_genename();	#a hash reference containing UniProtKB => genename conversions
 open my $FI, '<', $file or die "Could not open string file $file: $!\n";
 open my $FO, '>', $outfile or die "Could not open output file $outfile: $!\n";
 print $FO "GeneName1\tGeneName2\tDistance\n";
@@ -21,8 +22,13 @@ while(my $line = <$FI>){
 	my $distance = shift @words;
 	chomp($distance);
 
-	my $gene1 = manual_get_genename_by_UniProtKB($uniprot1);
-	my $gene2 = manual_get_genename_by_UniProtKB($uniprot2);
+	my $gene1;
+	$gene1 = $ug_ref->{$uniprot1} or $gene1 = 0;
+	chomp($gene1);
+	my $gene2;
+	$gene2 = $ug_ref->{$uniprot2} or $gene2 = 0;
+	chomp($gene2);
+
 	if ($gene1){
 		if ($gene2){	#both conversions exist
 			print $FO $gene1, "\t", $gene2, "\t", $distance, "\n";
