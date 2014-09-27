668702219ceb09f6bc15e2acf2a20fbfca4f6f9c
Hansaim Lim <han@naz.(none)>
2014-09-27 18:17:31 -0400

    fileout method for randomization of ppi added

--- perllib/create_object_files.pl
+++ perllib/create_object_files.pl
@@ -16,15 +16,33 @@ use warnings;
 #create_cmap_json("1000up");	#done
 #create_cmap_json("1000down");	#done
 #create_cmap_json("1000rand");	#done
-create_random_ppi_json(10);	#low shuffle numbers are for controlled experiments
-create_random_ppi_json(20);
-create_random_ppi_json(30);
-create_random_ppi_json(100000);
-create_random_ppi_json(1000000);
-create_random_ppi_json(2000000);
-create_random_ppi_json(3000000);
-create_random_ppi_json(4000000);
-create_random_ppi_json(5000000);
+print_random_ppi(20);	#testing the randomization mechanism
+print_random_ppi(30);
+#create_random_ppi_json(10);	#low shuffle numbers are for controlled experiments
+#create_random_ppi_json(20);
+#create_random_ppi_json(30);
+#create_random_ppi_json(100000);
+#create_random_ppi_json(1000000);
+#create_random_ppi_json(2000000);
+#create_random_ppi_json(3000000);
+#create_random_ppi_json(4000000);
+#create_random_ppi_json(5000000);
+sub print_random_ppi
+{
+	my $n = shift @_;
+	my $string_obj = new String();
+	my $string_shuffled_n = $string_obj->Randomize_String($n);
+	my $file = "./static/demo/randomppi".$n.".tsv";
+	my %hash = %$string_shuffled_n;
+	open my $FH, '>', $file or die "Could not open file $file: $!\n";
+	print $FH "Gene1\tGene2\tDistance\n";
+	foreach my $g1 (keys %hash){
+		foreach my $g2 (keys $hash{$g1}){
+			print $FH "$g1\t$g2\t$hash{$g1}{$g2}\n";
+		}
+	}
+	close $FH;
+}
 sub create_cmap_json
 {
 	#input : range
