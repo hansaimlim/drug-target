67569a2543229fe674476753255acf1433ecac91
Hansaim Lim <hansaimlim@gmail.com>
2014-10-17 03:18:55 -0400

    script for binding domain sequence (from CHEMBL MySQL) and PDB file download

--- protein/domain_sequences.pl
+++ protein/domain_sequences.pl
@@ -2,24 +2,77 @@
 use strict;
 use warnings;
 use DBI;
+use Data::Dumper;
 
 die "Usage: $0 <username> <password>\n" unless @ARGV == 2;
 
-my $driver = "MySQL"; 
-my $database = "chembl_16";
-my $dsn = "DBI:$driver:database=$database";
-my $userid = shift @ARGV;
+my $user = shift @ARGV;
 my $password = shift @ARGV;
 chomp($password);
 
 my $accession = 'P41595';
-my $dbh = DBI->connect($dsn, $userid, $password ) or die $DBI::errstr;
-my $sth = $dbh->prepare("SELECT * FROM component_sequences WHERE accession = '$accession'");
-$sth->execute() or die $DBI::errstr;
-print "Number of rows found :" + $sth->rows;
-while (my @row = $sth->fetchrow_array()) {
-   my ($first_name, $last_name ) = @row;
-   print "First Name = $first_name, Last Name = $last_name\n";
+my $start = 71;
+my $end = 380;
+my $dbh = connect_chembl_mysql($user, $password);
+my $subseq = get_substring($dbh, $accession, $start, $end);
+print $subseq, "\n";
+
+my @multi_domain_targets = get_accession_multidomain($dbh);	#Targets in this array appear to have multiple binding domains; 37 accessions for chembl_19
+sub connect_chembl_mysql {
+	my $user = shift @_;
+	my $pw = shift @_;
+	chomp($user);
+	chomp($pw);
+	my $driver = "mysql"; 
+	my $database = "chembl_19";
+	my $dsn = "DBI:$driver:database=$database";
+	my $dbh = DBI->connect($dsn, $user, $pw) or die $DBI::errstr;
+	return $dbh;
 }
-$sth->finish();
+
+sub get_substring {
+	my ($dbh, $accession, $start, $end) = @_;
+	my $sth = $dbh->prepare("SELECT SUBSTRING(sequence, $start, ($end-$start+1)) FROM component_sequences WHERE accession = '$accession'");
+	$sth->execute() or die $DBI::errstr;
+	my $num_row = $sth->rows;
+	my $sequence = $sth->fetchrow_array();
+	$sth->finish();
+	return $sequence;
+}
+
+sub get_num_site {
+
+}
+
+sub get_accession_multidomain {
+	my $dbh = shift @_;
+	my $sth = $dbh->prepare("SELECT cs.accession FROM component_sequences cs INNER JOIN component_domains cd ON cs.component_id = cd.component_id INNER JOIN site_components sc ON cs.component_id = sc.component_id AND cd.domain_id = sc.domain_id GROUP BY cs.accession HAVING (COUNT(DISTINCT(cd.compd_id)) > 1 AND COUNT(DISTINCT(cd.domain_id)) > 1)");
+	$sth->execute() or die $DBI::errstr;
+	my @accessions;
+	while(my $accession = $sth->fetchrow_array()){
+		chomp($accession);
+		push @accessions, $accession;
+	}
+	$sth->finish();
+	return @accessions;
+}
+
+sub make_dir
+{
+        #create a directory unless already created
+        my $dir = shift @_;
+        unless (-e $dir or mkdir $dir){
+                die "Unable to create $dir\n";
+        }
+        return;
+}
+
+sub dirname_add_slash
+{
+        #add a slash at the end of a directory name unless already attached
+        my $dir = shift @_;
+        $dir .= '/' unless $dir =~ m/\/$/;
+        return $dir;
+}
+
 exit;
